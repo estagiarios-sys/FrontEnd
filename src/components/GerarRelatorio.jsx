567bb866d7f2c360ba5bc033578b6810448a0d36
Gabriel <gabriel7oliveira22@gmail.com>
2024-08-22 14:00:46 -0300

    baixar pdf inicio implementação

--- src/components/GerarRelatorio.jsx
+++ src/components/GerarRelatorio.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useRef } from "react";
 import ModalSql from "./modais/ModalSql";
 import ModalPdf from "./modais/ModalPdf";
+import ModalExpo from "./modais/ModalExpo";
 import { useNavigate } from 'react-router-dom';
-import View from './PDF/pdfViewer';
+
 
 function GerarRelatorio({ selectedColumns, selectTable }) {
 
     const [isModalSqlOpen, setIsModalSqlOpen] = useState(false);
     const [isModalPdfOpen, setIsModalPdfOpen] = useState(false);
+    const [isModalExpoOpen, setIsModalExpoOpen] = useState(false);
 
-    const navigate = useNavigate();
-    const canvasRef = useRef(null);
-
-    const redirectToPDF = () => {
-        navigate('./pdfme');
+    const handleModalExpo = () => {
+        setIsModalExpoOpen(true);
     };
 
     const handleModalSql = () => {
@@ -24,12 +23,24 @@ function GerarRelatorio({ selectedColumns, selectTable }) {
         setIsModalPdfOpen(true);
     };
 
+    const closeModalExpo = () => {
+        setIsModalExpoOpen(false);
+    };
+
     const closeModalSql = () => {
         setIsModalSqlOpen(false);
     };
 
     const closeModalPdf = () => {
         setIsModalPdfOpen(false);
+    };
+
+
+    const navigate = useNavigate();
+    const canvasRef = useRef(null);
+
+    const redirectToPDF = () => {
+        navigate('./pdfme');
     };
 
     const fetchData = async (columns) => {
@@ -129,7 +140,7 @@ function GerarRelatorio({ selectedColumns, selectTable }) {
                 <div className="flex mr-36 justify-center items-center">
                     <div className="mx-2">
                         <div className="flex flex-col justify-center items-center">
-                            <button onClick={handleModalSql}>
+                            <button onClick={handleModalSql} className="flex flex-col justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
                                 </svg>
@@ -155,7 +166,7 @@ function GerarRelatorio({ selectedColumns, selectTable }) {
                     </div>
                     <div className="mx-2">
                         <div className="flex flex-col justify-center items-center">
-                            <button onClick={redirectToPDF}>
+                            <button onClick={redirectToPDF} className="flex flex-col justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
@@ -166,21 +177,22 @@ function GerarRelatorio({ selectedColumns, selectTable }) {
                     </div>
                     <div className="mx-2">
                         <div className="flex flex-col justify-center items-center">
-                            <button onClick={handleModalPdf}>
+                            <button onClick={handleModalPdf} className="flex flex-col justify-center items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                 </svg>
                                 <label htmlFor="mais">Prévia</label>
-
                             </button>
                         </div>
                     </div>
                     <div className="mx-2">
                         <div className="flex flex-col justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25a.75.75 0 00.75.75h16.5a.75.75 0 00.75-.75V16.5M7.5 12l4.5 4.5m0 0l4.5-4.5M12 3v13.5" />
-                            </svg>
-                            <label htmlFor="mais">Exportar</label>
+                            <button onClick={handleModalExpo} className="flex flex-col justify-center items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25a.75.75 0 00.75.75h16.5a.75.75 0 00.75-.75V16.5M7.5 12l4.5 4.5m0 0l4.5-4.5M12 3v13.5" />
+                                </svg>
+                                <label htmlFor="mais">Exportar</label>
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -190,6 +202,7 @@ function GerarRelatorio({ selectedColumns, selectTable }) {
             </div>
             <ModalSql isOpen={isModalSqlOpen} onClose={closeModalSql} />
             <ModalPdf isOpen={isModalPdfOpen} onClose={closeModalPdf} />
+            <ModalExpo isOpen={isModalExpoOpen} onClose={closeModalExpo} />
         </div>
     );
 }
