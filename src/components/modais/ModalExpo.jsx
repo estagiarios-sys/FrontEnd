d7a19d1346ac20ddebe3cf3e2a708fdc0529f4b1
Gabriel <gabriel7oliveira22@gmail.com>
2024-08-23 09:14:46 -0300

    arrumar um treco

--- src/components/modais/ModalExpo.jsx
+++ src/components/modais/ModalExpo.jsx
@@ -128,7 +128,7 @@ function ModalExpo({ isOpen, onClose }) {
                             <polyline points="7 10 12 15 17 10"></polyline>
                             <line x1="12" y1="15" x2="12" y2="3"></line>
                         </svg>
-                        <span>Baixar SVG</span>
+                        <span>Baixar CSV</span>
                     </button>
                 </div>
             </div>
