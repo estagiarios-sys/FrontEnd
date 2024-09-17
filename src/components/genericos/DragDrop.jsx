c96155e4e8bec146214be28662bc42f122ad59ec
guaranius-guh <gusta.antonius@gmail.com>
2024-09-17 11:44:55 -0300

    Adicionado Modal de Alerta ao não enviar imagem

--- src/components/genericos/DragDrop.jsx
+++ src/components/genericos/DragDrop.jsx
@@ -1,14 +1,23 @@
 //DragDrop do modal criar para seleção e visualização da imagem.
 
 import React, { useState, useRef, useEffect, useCallback } from "react";
+import ModalModal from '../modais/ModalModal';
 import PropTypes from 'prop-types';
 
-function DragDropFile({ onFileUpload }) {
+function DragDropFile({ isOpen, onClose, onFileUpload }) {
   const [dragActive, setDragActive] = useState(false);
   const [preview, setPreview] = useState(null);
   const inputRef = useRef(null);
+  const [modalMessage, setModalMessage] = useState('');
+  const [modalAlertImagem, setModalAlertImagem] = useState(false);
 
-//tratamento de tipo de arquivo enviado, setado como alert possivel mudança para modal:
+
+  const handleModalAlertImagem = () => {
+    setModalMessage('Por favor, envie um arquivo de imagem.');
+    setModalAlertImagem(true);
+  };
+
+  //tratamento de tipo de arquivo enviado, setado como alert possivel mudança para modal:
   const handleFile = useCallback((file) => {
     if (file.type.startsWith('image/')) {
       onFileUpload(file);
@@ -17,7 +26,7 @@ function DragDropFile({ onFileUpload }) {
       }
       setPreview(URL.createObjectURL(file));
     } else {
-      alert('Por favor, envie um arquivo de imagem.');
+      handleModalAlertImagem();
     }
   }, [onFileUpload, preview]);
 
@@ -60,54 +69,63 @@ function DragDropFile({ onFileUpload }) {
   }, [preview]);
 
   return (
-    <form
-      className="relative w-full max-w-[28rem] h-72 text-center"
-      onDragEnter={handleDrag}
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <input
-        ref={inputRef}
-        type="file"
-        id="input-file-upload"
-        className="hidden"
-        accept="image/*"
-        onChange={handleChange}
-      />
-      <label
-        htmlFor="input-file-upload"
-        className={`flex items-center justify-center h-20 border-2 border-dashed rounded-xl ${dragActive ? 'border-blue-500 bg-blue-50' : 'border-gray-300 bg-slate-50'}`}
+      <form
+        className="relative w-full max-w-[28rem] h-72 text-center"
+        onDragEnter={handleDrag}
+        onSubmit={(e) => e.preventDefault()}
       >
-        <div>
-          <p className="text-tiny">Arraste e solte sua imagem aqui</p>
-          <button
-            className="cursor-pointer p-1 text-base bg-transparent hover:underline text-tiny"
-            onClick={onButtonClick}
-            type="button"
-          >
-            Carregar arquivo
-          </button>
-        </div>
-      </label>
-      {dragActive && (
-        <div
-          className="absolute inset-0 w-full h-full rounded-xl"
-          onDragEnter={handleDrag}
-          onDragLeave={handleDrag}
-          onDragOver={handleDrag}
-          onDrop={handleDrop}
-        ></div>
-      )}
-      {preview && (
-        <div className="mt-4">
-          <h3>Visualização da Imagem:</h3>
-          <img
-            src={preview}
-            alt="Pré-visualização"
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
-        </div>
-      )}
-    </form>
+        <input
+          ref={inputRef}
+          type="file"
+          id="input-file-upload"
+          className="hidden"
+          accept="image/*"
+          onChange={handleChange}
+        />
+        <label
+          htmlFor="input-file-upload"
+          className={`flex items-center justify-center h-20 border-2 border-dashed rounded-xl ${dragActive ? 'border-blue-500 bg-blue-50' : 'border-gray-300 bg-slate-50'}`}
+        >
+          <div>
+            <p className="text-tiny">Arraste e solte sua imagem aqui</p>
+            <button
+              className="cursor-pointer p-1 text-base bg-transparent hover:underline text-tiny"
+              onClick={onButtonClick}
+              type="button"
+            >
+              Carregar arquivo
+            </button>
+          </div>
+        </label>
+        <ModalModal modalType="ATENÇÃO"
+          isOpen={modalAlertImagem}
+          onClose={() => setModalAlertImagem(false)}
+          onConfirm={onClose}
+          confirmText="Fechar"
+          message={modalMessage}
+        />
+        {dragActive && (
+          <div
+            className="absolute inset-0 w-full h-full rounded-xl"
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
+          ></div>
+        )}
+        {preview && (
+          <div className="mt-4">
+            <h3>Visualização da Imagem:</h3>
+            <img
+              src={preview}
+              alt="Pré-visualização"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+            
+          </div>
+        )}
+      </form>
+      
   );
 }
 
