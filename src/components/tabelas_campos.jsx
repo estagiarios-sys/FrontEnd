018dfb318b390383afb4f4b7abddbe5333a7e825
Atáliton Neto <134219915+ataliton@users.noreply.github.com>
2024-09-18 14:19:11 -0300

    Update tabelas_campos.jsx

--- src/components/tabelas_campos.jsx
+++ src/components/tabelas_campos.jsx
@@ -369,7 +369,7 @@ function TabelaCampos({ onDataChange, handleAllLeftClick, passHandleLoadFromLoca
                 <path fill="currentColor" fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd" />
               </svg>
             </button>
-            <div className='info-texto'> ola Selecione a tabela que será relacionada com a principal</div>
+            <div className='info-texto'>Selecione a tabela que será relacionada com a principal</div>
           </div>
         </div>
       </div>
