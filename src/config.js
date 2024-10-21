6899ebd3607b83c0691fcbe72f1dd2f4265d6400
Pedro Santos <pedropepe908@gmail.com>
2024-10-21 11:26:05 -0300

    testando com rotas origins

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const urlOrigin = window.location.hostname;
+const urlOrigin = window.location.origin;
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const linkFinal = `http://${urlOrigin}${API_URL}`;
+export const linkFinal = `${urlOrigin}${API_URL}`;
