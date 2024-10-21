bdd9e4f30dc4804ca652ee59d3566b4c6ad93dfe
Pedro Santos <pedropepe908@gmail.com>
2024-10-21 11:18:28 -0300

    teste de deploy

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
 FROM node:18
 
-
 WORKDIR /app
-
 
 COPY package*.json ./
 
 RUN npm install
 
 COPY . .
-
 
 RUN npm run build
 
--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ const urlOrigin = window.location.hostname;
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const linkFinal = `http://${urlOrigin}:8082${API_URL}`;
+export const linkFinal = `http://${urlOrigin}${API_URL}`;
