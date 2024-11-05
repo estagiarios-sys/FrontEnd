4c43e6c9fd2ed3bd4271cb513d4a0ecf19227a7b
Pedro Santos <pedropepe908@gmail.com>
2024-11-05 17:03:35 -0300

    Adicionando server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/reports', express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/reports/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
