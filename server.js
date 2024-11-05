02e968961f11cb12de1c2a5433216e2feb9ee175
Pedro Santos <pedropepe908@gmail.com>
2024-11-05 17:14:22 -0300

    Adicionando server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use('/reports', express.static(path.join(__dirname, 'build')));
 
-app.get('/reports/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
