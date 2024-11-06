3065e6fd4d90d1bb921fe39489690efeaf8ad579
Gabriel de Oliveira <148984768+GabriD-0@users.noreply.github.com>
2024-11-06 14:43:46 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
-// const urlOrigin = window.location.origin;
+const urlOrigin = window.location.origin;
 
-const urlOrigin = window.location.hostname;
-
+//const urlOrigin = window.location.hostname;
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-//export const linkFinal = `${urlOrigin}${API_URL}`;
+export const linkFinal = `${urlOrigin}${API_URL}`;
 
-export const linkFinal = `http://${urlOrigin}:8082${API_URL}`;
+//export const linkFinal = `http://${urlOrigin}:8082${API_URL}`;
