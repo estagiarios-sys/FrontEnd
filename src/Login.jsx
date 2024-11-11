38dadf9434831312c86ba92456d1eca905a588bd
Pedro <pedropepe908@gmail.com>
2024-11-11 17:07:50 -0300

    Trabalhando com recebimento de tokens

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,21 +1,38 @@
 import imagem from "./imagens/image.png";
+import { useState } from "react";
+import axios from 'axios';
 
 export default function Login() {
+  const [name, setname] = useState('');
+  const [password, setpassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post('/api/auth/login', {name, password});
+      const {token} = res.data;
+      localStorage.setItem('token', token);
+      console.log(token)
+      window.location.href = '/reports';
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-slate-200">
       <div className="w-1/4 rounded-md shadow-xl bg-white">
         <div className="flex flex-col items-center justify-center">
-          <img src={imagem} className="mt-5 w-2/3" />
+          <img src={imagem} className="mt-10 w-2/3" />
         </div>
         <div className="flex flex-col m-10 text-2xl">
-          <form method="POST">
+          <form onSubmit={handleSubmit} method="POST">
             <div className="mb-4">
               <label className="block text-gray-700 text-lg font-bold mb-2">
                 Usuário:
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  {/* User icon */}
                   <svg
                     className="h-5 w-5 text-gray-400"
                     fill="currentColor"
@@ -31,9 +48,11 @@ export default function Login() {
                 </div>
                 <input
                   type="text"
-                  name="username"
+                  name="name"
                   className="pl-10 w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Digite seu nome..."
+                  value={name}
+                  onChange={(e) => setname(e.target.value)}
                 />
               </div>
             </div>
@@ -43,7 +62,6 @@ export default function Login() {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  {/* Lock icon */}
                   <svg
                     className="h-5 w-5 text-gray-400"
                     fill="currentColor"
@@ -61,6 +79,8 @@ export default function Login() {
                   name="password"
                   className="pl-10 w-full border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="Digite sua senha..."
+                  value={password}
+                  onChange={(e) => setpassword(e.target.value)}
                 />
               </div>
             </div>
