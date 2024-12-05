d67aed959991bdf7e8cc2515d41d9edcfdd3d6a9
GabriD-0 <gabriel7oliveira22@gmail.com>
2024-12-05 15:28:36 -0300

    Adicionando notificação no editar

--- src/components/GerarRelatorio.jsx
+++ src/components/GerarRelatorio.jsx
@@ -473,6 +473,16 @@ function GenerateReport({ selectedColumns, selectTable, selectedRelatedTables, s
     const handleImgPdf = (img) => {
         // console.log("Componente Pai: Atualizando imagem para:", img);
         setImgPdf(img);
+
+        if (img) {
+            const reader = new FileReader();
+            reader.readAsDataURL(img);
+            reader.onloadend = () => {
+                setBase64Image(reader.result);
+            };
+        } else {
+            setBase64Image('');
+        }
     };
 
 
@@ -620,7 +630,13 @@ function GenerateReport({ selectedColumns, selectTable, selectedRelatedTables, s
                         funcao={() => openModal('editar')}
                         nome={"Editar"}
                         icon={"m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"}
-                    />
+                    >                     
+                        {(base64Image || imgPdf || (titlePdf !== '' && titlePdf !== null && titlePdf !== undefined)) && (
+                            <span className="absolute -top-2 -right-1 bg-custom-vermelho text-white rounded-full text-xs w-4 h-4 flex justify-center items-center">  
+                            !
+                            </span>
+                        )}
+                    </IconsTemplate>
                     <IconsTemplate
                         funcao={() => {
                             if (tableData.length === 0) {
